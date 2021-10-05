d681b83d55f82f00f76e2bc0bcab825e4df9d2ff
Tira Sundara <sundaralinus@gmail.com>
2021-10-05 20:44:34 +0700

    Refactor Repeat. Now Repeat function receives two arguments: s string, n int

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,13 +1,11 @@
 package iteration
 
-const repeatCount = 5
-
-// Repeat returns the repeated (5 times) supplied char
-func Repeat(char string) string {
+// Repeat repeats the string s by n times, and return it
+func Repeat(s string, n int) string {
 	var repeated string
 
-	for i := 0; i < repeatCount; i++ {
-		repeated += char
+	for i := 0; i < n; i++ {
+		repeated += s
 	}
 
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -6,22 +6,33 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	want := "aaaaa"
+	t.Run("returns repeated string correctly", func(t *testing.T) {
+		got := Repeat("a", 5)
+		want := "aaaaa"
 
-	if got != want {
-		t.Errorf("expected %q but got %q", want, got)
-	}
+		if got != want {
+			t.Errorf("expected %q but got %q", want, got)
+		}
+	})
+
+	t.Run("returns empty string when n is less than 1", func(t *testing.T) {
+		got := Repeat("a", -1)
+		want := ""
+
+		if got != want {
+			t.Errorf("expected %q but got %q", want, got)
+		}
+	})
 }
 
 func ExampleRepeat() {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	fmt.Println(repeated)
 	// Output: aaaaa
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
