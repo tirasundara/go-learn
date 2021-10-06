740652de5f655521d9820f279da166bcd2caf124
Tira Sundara <sundaralinus@gmail.com>
2021-10-06 15:36:32 +0700

    Refactor SummAll()

--- slices/sum.go
+++ slices/sum.go
@@ -8,14 +8,9 @@ func Sum(numbers []int) (sum int) {
 	return
 }
 
-func SumAll(numbersToSum ...[]int) []int {
-	length := len(numbersToSum)
-	sums := make([]int, length)
-
-	for i, numbers := range numbersToSum {
-		sum := Sum(numbers)
-		sums[i] = sum
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
-
-	return sums
+	return
 }
