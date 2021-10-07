e5800ca0087a29a57f8727dc338a6695d5cf5b67
Tira Sundara <sundaralinus@gmail.com>
2021-10-07 18:00:02 +0700

    Refactor Perimeter() function: Make it as Rectangle.Perimeter()

--- structs/shape.go
+++ structs/shape.go
@@ -11,8 +11,8 @@ type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 func (r Rectangle) Area() float64 {
--- structs/shape_test.go
+++ structs/shape_test.go
@@ -3,7 +3,8 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(Rectangle{10.0, 10.0})
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
