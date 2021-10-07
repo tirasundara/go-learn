a619194f8e975501a43eceac2132989eba5994e7
Tira Sundara <sundaralinus@gmail.com>
2021-10-07 18:40:05 +0700

    Introduced Table Driven Tests

--- structs/shape_test.go
+++ structs/shape_test.go
@@ -13,6 +13,14 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10.0}, 314.1592653589793},
+	}
+
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
@@ -22,17 +30,13 @@ func TestArea(t *testing.T) {
 		}
 	}
 
-	t.Run("Rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		want := 72.0
+	for _, areaTest := range areaTests {
+		t.Run("Rectangle", func(t *testing.T) {
+			checkArea(t, areaTest.shape, areaTest.want)
+		})
 
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("Circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := 314.1592653589793
-
-		checkArea(t, circle, want)
-	})
+		t.Run("Circle", func(t *testing.T) {
+			checkArea(t, areaTest.shape, areaTest.want)
+		})
+	}
 }
