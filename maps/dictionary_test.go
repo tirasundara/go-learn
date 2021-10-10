6337ba2ab33226b25a57c3175e4cd4cf3a8791e7
Tira Sundara <sundaralinus@gmail.com>
2021-10-10 19:30:43 +0700

    Return error when the definition of the requested word is not exist in the Dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "Could not find you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertString(t, err.Error(), want)
+	})
 }
 
 func assertString(t testing.TB, got, want string) {
