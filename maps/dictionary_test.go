c4fb76c4048904ace27c477b7142717642cc7249
Tira Sundara <sundaralinus@gmail.com>
2021-10-10 20:59:35 +0700

    Introduced Dictionary.Delete() method

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -67,6 +67,19 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "this is just a test"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
+}
+
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
