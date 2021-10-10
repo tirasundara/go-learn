ccbfaf18dea6f91d0a480f163bb26ce064412d45
Tira Sundara <sundaralinus@gmail.com>
2021-10-10 20:34:01 +0700

    Add Dictionary.Add() method

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,14 +4,31 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrorNotFound = errors.New("Could not find you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
-
 	if !ok {
-		return "", ErrorNotFound
+		return "", ErrNotFound
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
