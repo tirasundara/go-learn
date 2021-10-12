77037224f1bff850520cec5945d6b43737b1cec1
Tira Sundara <sundaralinus@gmail.com>
2021-10-12 20:53:20 +0700

    Create working Countdown() function

---
+++ mocks/countdown.go
@@ -0,0 +1,14 @@
+package mocks
+
+import (
+	"fmt"
+	"io"
+)
+
+func Countdown(out io.Writer) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	fmt.Fprint(out, "Go!")
+}
---
+++ mocks/countdown_test.go
@@ -0,0 +1,19 @@
+package mocks
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := bytes.Buffer{}
+
+	Countdown(&buffer)
+
+	got := buffer.String()
+	want := "3\n2\n1\nGo!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
