4ed97e82b4a50000a1469a71ac1232653ceb70e2
Tira Sundara <sundaralinus@gmail.com>
2021-10-14 20:13:30 +0700

    Introduced go routines and channel to make CheckWebsites() run much faster

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -2,11 +2,24 @@ package concurrency
 
 type WebsiteChecker func(url string) bool
 
+type result struct {
+	url  string
+	isUp bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{url: u, isUp: wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.url] = result.isUp
 	}
 
 	return results
