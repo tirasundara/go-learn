fc368001ca448489e7724f380fdf474cc6e01ec1
Tira Sundara <sundaralinus@gmail.com>
2021-10-18 21:33:49 +0700

    Reflection iteration-6. walk() function handles Map argument

--- reflections/walk.go
+++ reflections/walk.go
@@ -5,22 +5,21 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	v := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
-
 	switch v.Kind() {
 	case reflect.String:
 		fn(v.String())
 	case reflect.Struct:
-		numberOfValues = v.NumField()
-		getField = v.Field
+		for i := 0; i < v.NumField(); i++ {
+			walk(v.Field(i).Interface(), fn)
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = v.Len()
-		getField = v.Index
-	}
-
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
+		for i := 0; i < v.Len(); i++ {
+			walk(v.Index(i).Interface(), fn)
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			walk(v.MapIndex(key).Interface(), fn)
+		}
 	}
 }
 
