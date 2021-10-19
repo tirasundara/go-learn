46e6d7297dba49a990fec2f24a380be0da492eb3
Tira Sundara <sundaralinus@gmail.com>
2021-10-19 07:29:14 +0700

    Refactor the counter_test.go, introduce NewCounter() which return *Counter to avoid passing a copy of Counter, because it has mutex

--- sync/counter_test.go
+++ sync/counter_test.go
@@ -7,17 +7,17 @@ import (
 
 func TestCount(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		assertCounter(t, &counter, 3)
+		assertCounter(t, counter, 3)
 	})
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		counter := NewCounter()
 		wg := sync.WaitGroup{}
 		wg.Add(wantedCount)
 
@@ -30,7 +30,7 @@ func TestCount(t *testing.T) {
 
 		wg.Wait()
 
-		assertCounter(t, &counter, wantedCount)
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
