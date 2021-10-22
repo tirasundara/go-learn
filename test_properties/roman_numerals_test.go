eb333c8e36e501c632fd5f1330dd4dbb9aa0d5fa
Tira Sundara <sundaralinus@gmail.com>
2021-10-22 08:41:31 +0700

    Refactor the roman_numerals_test.go to use property based tests

--- test_properties/roman_numerals_test.go
+++ test_properties/roman_numerals_test.go
@@ -3,21 +3,22 @@ package testproperties
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, testCase := range cases {
+		t.Run(testCase.Description, func(t *testing.T) {
+			got := ConvertToRoman(testCase.Arabic)
 
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+			if got != testCase.Want {
+				t.Errorf("got %q, want %q", got, testCase.Want)
+			}
+		})
+	}
 }
